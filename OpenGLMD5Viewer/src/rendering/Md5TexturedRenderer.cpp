8990d2f1e46c6857c03c3f8d0bca53c9a204132f
Jacques Diringer <jacquesdiringer@msn.com>
2012-06-18 10:37:57 +0800

    ...

--- OpenGLMD5Viewer/src/rendering/Md5TexturedRenderer.cpp
+++ OpenGLMD5Viewer/src/rendering/Md5TexturedRenderer.cpp
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../core/utils/subLoaders.h"
+#include "../core/utils/shaderLoader.h"
 
 #include "Md5TexturedRenderer.h"
 
--- OpenGLMD5Viewer/ui_openglmd5viewer.h
+++ OpenGLMD5Viewer/ui_openglmd5viewer.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'openglmd5viewer.ui'
 **
-** Created: Mon 18. Jun 10:25:19 2012
+** Created: Mon 18. Jun 10:35:20 2012
 **      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
